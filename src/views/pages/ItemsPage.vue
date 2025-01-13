d7b2e004201b2cf170b059e2fe346cf09ad8bef1
mahmod <mahmodmos47@gmail.com>
2025-01-13 16:48:11 +0200

    Items & Customers

--- src/views/pages/ItemsPage.vue
+++ src/views/pages/ItemsPage.vue
@@ -20,109 +20,107 @@ const navigateToDashboard = () => {
     router.push('/');
 };
 
-const testData = ref([]);
-testData.value = [
-    {
-        barcode: 'AAAAAA SSSSSSSSS',
-        brand: null,
-        itemGroup: 'BTY',
-        sapDescAr: 'ACDelco N55D23R إيه سي ديلكو بطارية',
-        sapDescEn: 'ACDelco N55D23R إيه سي ديلكو بطارية',
-        sapItem: 'CPBATACD006',
-        sapPlant: 'MC01',
-        price: 236.1,
-        id: 29586,
-        uniqueIdentifier: '914b873f-d550-4c19-bbd1-d177767ab30c',
-        createdAt: '2025-01-05T16:06:10.069689Z'
-    },
-    {
-        barcode: 'BBBBBB SSSSSSSSS',
-        brand: null,
-        itemGroup: 'BTY',
-        sapDescAr: 'ACDelco N55D23L إيه سي ديلكو بطارية',
-        sapDescEn: 'ACDelco N55D23L إيه سي ديلكو بطارية',
-        sapItem: 'CPBATACD005',
-        sapPlant: 'MC01',
-        price: 236.1,
-        id: 29587,
-        uniqueIdentifier: '06806bf4-1e1d-44c3-bd94-c20c67a83442',
-        createdAt: '2025-01-05T16:06:10.069689Z'
-    }
-];
-const formatCurrency = (value) => {
-    return new Intl.NumberFormat('ar-SA', { style: 'currency', currency: 'SAR' }).format(value);
-};
+import apiClient from '@/api/apiClient';
 
-const totalPrice = computed(() => {
-    return testData.value.reduce((sum, item) => sum + item.price, 0);
-});
+const ItemsData = ref([]);
+
+const getItems = async () => {
+    try {
+        console.log('response');
+        const response = await apiClient.get(`/Items`);
+        ItemsData.value = response.data.data;
+        console.log(ItemsData.value);
+    } catch (err) {
+        console.log(err);
+    }
+};
+getItems();
+
+const Rtl = ref();
+const getRtl = localStorage.getItem('Rtl');
+Rtl.value = getRtl;
+console.log(Rtl.value);
 </script>
 
 <template>
-    <div>
-        <DataTable :value="testData" tableStyle="min-width: 50rem">
-            <template #header>
+    <div class="">
+        <div class="card">
+            <div class="flex flex-column md:flex-row md:align-items-start md:justify-content-between mb-3">
                 <div class="flex flex-wrap items-center justify-between gap-2">
-                    <span class="text-xl font-bold">Products</span>
-                    <Button icon="pi pi-refresh" rounded raised />
+                    <Button icon="pi pi-refresh" @click="getItems" rounded raised />
                 </div>
-            </template>
+            </div>
 
-            <Column field="sapItem" header="Item Code">
-                <template #body="slotProps">
-                    <div class="flex flex-wrap items-center justify-between gap-2">
-                        <span>{{ slotProps.data.sapItem }}</span>
-                    </div>
-                </template>
-            </Column>
+            <DataTable :value="ItemsData" dataKey="id" paginator :rows="5" responsiveLayout="scroll" v-model:filters="filterSalesTable">
+                <template #empty> No products found.</template>
+                <Column field="barcode" header="barcode" sortable :headerStyle="{ minWidth: '12rem' }">
+                    <template #body="{ data }">
+                        <span class="p-column-title">barcode</span>
+                        <div class="flex">{{ data.barcode }}</div>
+                    </template>
+                </Column>
 
-            <Column field="sapDescEn" header="Description">
-                <template #body="slotProps">
-                    <div class="flex flex-wrap items-center justify-between gap-2">
-                        <span>{{ slotProps.data.sapDescEn }}</span>
-                    </div>
-                </template>
-            </Column>
+                <Column field="brand" header="brand" sortable :headerStyle="{ minWidth: '10rem' }">
+                    <template #body="{ data }">
+                        <span class="p-column-title">brand</span>
+                        <div class="flex">{{ data.brand }}</div>
+                    </template>
+                </Column>
 
-            <Column field="price" header="Price">
-                <template #body="slotProps">
-                    <div class="flex flex-wrap items-center justify-between gap-2">
-                        <span> {{ formatCurrency(slotProps.data.price) }}</span>
-                    </div>
-                </template>
-            </Column>
+                <Column field="id" header="id" sortable :headerStyle="{ minWidth: '10rem' }">
+                    <template #body="{ data }">
+                        <span class="p-column-title">id</span>
+                        <div class="flex">{{ data.id }}</div>
+                    </template>
+                </Column>
 
-            <Column field="itemGroup" header="Item Group">
-                <template #body="slotProps">
-                    <div class="flex flex-wrap items-center justify-between gap-2">
-                        <span>{{ slotProps.data.itemGroup }}</span>
-                    </div>
-                </template>
-            </Column>
+                <Column field="itemGroup" header="itemGroup" sortable :headerStyle="{ minWidth: '10rem' }">
+                    <template #body="{ data }">
+                        <span class="p-column-title">itemGroup</span>
+                        <div class="flex">{{ data.itemGroup }}</div>
+                    </template>
+                </Column>
 
-            <Column field="barcode" header="Barcode">
-                <template #body="slotProps">
-                    <div class="flex flex-wrap items-center justify-between gap-2">
-                        <span>{{ slotProps.data.barcode }}</span>
-                    </div>
-                </template>
-            </Column>
+                <Column field="price" header="price" sortable :headerStyle="{ minWidth: '10rem' }">
+                    <template #body="{ data }">
+                        <span class="p-column-title">price</span>
+                        <div class="flex">{{ data.price }}</div>
+                    </template>
+                </Column>
 
-            <Column field="sapPlant" header="Plant">
-                <template #body="slotProps">
-                    <div class="flex flex-wrap items-center justify-between gap-2">
-                        <span>{{ slotProps.data.sapPlant }}</span>
-                    </div>
-                </template>
-            </Column>
+                <Column field="sapDescAr" v-if="Rtl" header="sapDescAr" sortable :headerStyle="{ minWidth: '10rem' }">
+                    <template #body="{ data }">
+                        <span class="p-column-title">sapDescAr</span>
 
-            <template #footer>
-                <div class="flex justify-between items-center flex-column gap-2">
-                    <span>In total there are {{ testData.length }} products.</span>
-                    <span class="font-bold">Total Price: {{ formatCurrency(totalPrice) }}</span>
-                </div>
-            </template>
-        </DataTable>
+                        <div class="flex">{{ data.sapDescAr }}</div>
+                    </template>
+                </Column>
+
+                <Column field="sapDescEn" v-else header="sapDescEn" sortable :headerStyle="{ minWidth: '10rem' }">
+                    <template #body="{ data }">
+                        <span class="p-column-title">sapDescEn</span>
+
+                        <div class="flex">{{ data.sapDescEn }}</div>
+                    </template>
+                </Column>
+
+                <Column field="sapItem" header="sapItem" sortable :headerStyle="{ minWidth: '10rem' }">
+                    <template #body="{ data }">
+                        <span class="p-column-title">sapItem</span>
+
+                        <div class="flex">{{ data.sapItem }}</div>
+                    </template>
+                </Column>
+
+                <Column field="sapPlant" header="sapPlant" sortable :headerStyle="{ minWidth: '10rem' }">
+                    <template #body="{ data }">
+                        <span class="p-column-title">sapPlant</span>
+
+                        <div class="flex">{{ data.sapPlant }}</div>
+                    </template>
+                </Column>
+            </DataTable>
+        </div>
     </div>
 </template>
 
