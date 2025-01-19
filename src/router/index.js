9c372a7e81afaca1da542ab08ff6aa615f7cb5d3
Ahmed Ehab <ahmedehab25299@gmail.com>
2025-01-19 19:44:47 +0000

    changing path name for page

--- src/router/index.js
+++ src/router/index.js
@@ -479,7 +479,7 @@ const router = createRouter({
                     component: () => import('@/views/pages/Invoice.vue')
                 },
                 {
-                    path: '/Quickinvoice',
+                    path: '/invoices/quick-invoice',
                     name: 'Quickinvoice',
                     component: () => import('@/views/pages/invoice/QuickInvoice.vue')
                 },
@@ -582,27 +582,27 @@ const router = createRouter({
     }
 });
 
-router.beforeEach(async (to, from, next) => {
-    console.log(`From: ${from.path}, To: ${to.path}`);
-    const mainStore = useMainStore();
-    const token = sessionStorage.getItem('Token') || localStorage.getItem('token');
+// router.beforeEach(async (to, from, next) => {
+//     console.log(`From: ${from.path}, To: ${to.path}`);
+//     const mainStore = useMainStore();
+//     const token = sessionStorage.getItem('Token') || localStorage.getItem('token');
 
-    if (token && mainStore.pageTree.length == 0) {
-        await mainStore.getMenu();
-    }
+//     if (token && mainStore.pageTree.length == 0) {
+//         await mainStore.getMenu();
+//     }
 
-    const isAllowed = mainStore.accessAllowed(to.path);
-    console.log(`Navigating to: ${to.path}, Allowed: ${isAllowed}`);
+//     const isAllowed = mainStore.accessAllowed(to.path);
+//     console.log(`Navigating to: ${to.path}, Allowed: ${isAllowed}`);
 
-    if (!token && to.name !== 'login') {
-        next({ name: 'login' });
-    } else if (token && to.name === 'login') {
-        next({ name: 'e-commerce' });
-    } else if (token && !isAllowed) {
-        next({ name: 'notfound' });
-    } else {
-        next();
-    }
-});
+//     if (!token && to.name !== 'login') {
+//         next({ name: 'login' });
+//     } else if (token && to.name === 'login') {
+//         next({ name: 'e-commerce' });
+//     } else if (token && !isAllowed) {
+//         next({ name: 'notfound' });
+//     } else {
+//         next();
+//     }
+// });
 
 export default router;
