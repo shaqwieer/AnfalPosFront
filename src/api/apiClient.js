00311e3da891331d122b5fda6daec15a8f67ace0
Ahmed Ehab <ahmedehab25299@gmail.com>
2025-03-09 00:11:04 +0200

    fixing refresh token issue

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -6,139 +6,139 @@ import { getActivePinia } from 'pinia';
 import { useLoaderDataStore } from '../stores/loader';
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API,
-    headers: {
-        Accept: 'application/json',
-    },
+  baseURL: import.meta.env.VITE_API,
+  headers: {
+    Accept: 'application/json'
+  }
 });
 
 const handleHttpResponseOrError = (status, data) => {
-    const pinia = getActivePinia();
-    if (!pinia) return;
+  const pinia = getActivePinia();
+  if (!pinia) return;
 
-    const mainStore = useMainStore(pinia);
+  const mainStore = useMainStore(pinia);
 
-    switch (status) {
-        case 200:
-            break;
-        case 400:
-            console.error('Bad Request:', data);
-            break;
-        case 401:
-            if (getFromLocalStorage('rememberMe') === false) {
-                mainStore.logout();
-            }
-            break;
-        case 403:
-            console.error('Forbidden:', data);
-            router.push('/');
-            break;
-        case 404:
-            console.error('Not Found:', data);
-            break;
-        case 500:
-            console.error('Internal Server Error:', data);
-            break;
-        default:
-            console.error('HTTP response or error:', status, data);
-            break;
-    }
+  switch (status) {
+    case 200:
+      break;
+    case 400:
+      console.error('Bad Request:', data);
+      break;
+    case 401:
+      if (getFromLocalStorage('rememberMe') == 'false') {
+        mainStore.logout();
+      }
+      break;
+    case 403:
+      console.error('Forbidden:', data);
+      router.push('/');
+      break;
+    case 404:
+      console.error('Not Found:', data);
+      break;
+    case 500:
+      console.error('Internal Server Error:', data);
+      break;
+    default:
+      console.error('HTTP response or error:', status, data);
+      break;
+  }
 };
 
 apiClient.interceptors.request.use(
-    (config) => {
-        const pinia = getActivePinia();
-        if (pinia) {
-            const loaderStore = useLoaderDataStore(pinia);
-            loaderStore.showLoader();
-        }
-
-        const token = getFromLocalStorage('token') || sessionStorage.getItem('Token');
-        const preferredLanguage = getFromLocalStorage('Language')
-            ? {
-                  name: getFromLocalStorage('Language').name,
-                  value: getFromLocalStorage('Language').value,
-              }
-            : { name: 'English', value: 'en' };
-
-        config.headers['Accept-Language'] = preferredLanguage.value;
-
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-
-        return config;
-    },
-    (error) => {
-        const pinia = getActivePinia();
-        if (pinia) {
-            const loaderStore = useLoaderDataStore(pinia);
-            loaderStore.hideLoader();
-        }
-        return Promise.reject(error);
+  (config) => {
+    const pinia = getActivePinia();
+    if (pinia) {
+      const loaderStore = useLoaderDataStore(pinia);
+      loaderStore.showLoader();
     }
+
+    const token = getFromLocalStorage('token') || sessionStorage.getItem('Token');
+    const preferredLanguage = getFromLocalStorage('Language')
+      ? {
+          name: getFromLocalStorage('Language').name,
+          value: getFromLocalStorage('Language').value
+        }
+      : { name: 'English', value: 'en' };
+
+    config.headers['Accept-Language'] = preferredLanguage.value;
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    const pinia = getActivePinia();
+    if (pinia) {
+      const loaderStore = useLoaderDataStore(pinia);
+      loaderStore.hideLoader();
+    }
+    return Promise.reject(error);
+  }
 );
 
 apiClient.interceptors.response.use(
-    (response) => {
-        const pinia = getActivePinia();
-        if (pinia) {
-            const loaderStore = useLoaderDataStore(pinia);
-            loaderStore.hideLoader();
-        }
-        handleHttpResponseOrError(response.status, response.data);
-        return response;
-    },
-    async (error) => {
-        const pinia = getActivePinia();
-        if (pinia) {
-            const loaderStore = useLoaderDataStore(pinia);
-            loaderStore.hideLoader();
-        }
-
-        const { response } = error;
-
-        if (response) {
-            const token = getFromLocalStorage('token');
-            const rememberMe = getFromLocalStorage('rememberMe');
-
-            if (response.status === 401 && !error.config._retry && token && rememberMe) {
-                error.config._retry = true;
-
-                const newToken = await refreshAuthToken();
-                if (newToken) {
-                    axios.defaults.headers.common['Authorization'] = `Bearer ${newToken}`;
-                    return apiClient(error.config);
-                }
-            }
-
-            handleHttpResponseOrError(response.status, response.data);
-        } else if (error.request) {
-            console.error('No response received:', error.request);
-        } else {
-            console.error('Error setting up the request:', error.message);
-        }
-
-        return Promise.reject(error);
+  (response) => {
+    const pinia = getActivePinia();
+    if (pinia) {
+      const loaderStore = useLoaderDataStore(pinia);
+      loaderStore.hideLoader();
     }
+    handleHttpResponseOrError(response.status, response.data);
+    return response;
+  },
+  async (error) => {
+    const pinia = getActivePinia();
+    if (pinia) {
+      const loaderStore = useLoaderDataStore(pinia);
+      loaderStore.hideLoader();
+    }
+
+    const { response } = error;
+
+    if (response) {
+      const token = getFromLocalStorage('token');
+      const rememberMe = getFromLocalStorage('rememberMe');
+
+      if (response.status === 401 && !error.config._retry && token && rememberMe == 'true') {
+        error.config._retry = true;
+
+        const newToken = await refreshAuthToken();
+        if (newToken) {
+          axios.defaults.headers.common['Authorization'] = `Bearer ${newToken}`;
+          return apiClient(error.config);
+        }
+      }
+
+      handleHttpResponseOrError(response.status, response.data);
+    } else if (error.request) {
+      console.error('No response received:', error.request);
+    } else {
+      console.error('Error setting up the request:', error.message);
+    }
+
+    return Promise.reject(error);
+  }
 );
 
 export default apiClient;
 
 async function refreshAuthToken() {
-    try {
-        const response = await axios.post(`${import.meta.env.VITE_API}/auth/refresh-token`, {
-            token: getFromLocalStorage('refreshToken'),
-        });
+  try {
+    const response = await axios.post(`${import.meta.env.VITE_API}/auth/refresh-token`, {
+      token: getFromLocalStorage('refreshToken')
+    });
 
-        const { token } = response.data;
-        if (token) {
-            localStorage.setItem('token', token);
-            return token;
-        }
-        return null;
-    } catch (error) {
-        console.error('Failed to refresh token:', error);
-        return null;
+    const { token } = response.data;
+    if (token) {
+      localStorage.setItem('token', token);
+      return token;
     }
+    return null;
+  } catch (error) {
+    console.error('Failed to refresh token:', error);
+    return null;
+  }
 }
--- src/stores/mainStore.js
+++ src/stores/mainStore.js
@@ -5,141 +5,140 @@ import apiClient from '../api/apiClient';
 import router from '@/router/index.js';
 import { extractToValues, extractRouteList, removeEmptyItems } from '@/utilities/treeHelpers.js';
 import { handleError } from '@/utilities/errorHandler';
-import {jwtDecode} from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 
 export const useMainStore = defineStore({
-    id: 'main',
-    state: () => ({
-        isRTL: getFromLocalStorage('Rtl') ? getFromLocalStorage('Rtl') : false,
-        language: getFromLocalStorage('Language') ? getFromLocalStorage('Language') : { name: 'English', code: 'en' }, // Default language
-        loading: useLoadingStore(),
-        error: '',
-        pageTree: [],
-        pathsList: ['/pages/notfound', '/auth/login', '/', '/auth/access'],
-        routeList: [],
-        userInfo: []
-    }),
-    getters: {
-        accessAllowed: (state) => (path) => {
-            // Check exact paths
-            if (state.pathsList.includes(path)) {
-                return true;
-            }
+  id: 'main',
+  state: () => ({
+    isRTL: getFromLocalStorage('Rtl') ? getFromLocalStorage('Rtl') : false,
+    language: getFromLocalStorage('Language') ? getFromLocalStorage('Language') : { name: 'English', code: 'en' }, // Default language
+    loading: useLoadingStore(),
+    error: '',
+    pageTree: [],
+    pathsList: ['/pages/notfound', '/auth/login', '/', '/auth/access'],
+    routeList: [],
+    userInfo: []
+  }),
+  getters: {
+    accessAllowed: (state) => (path) => {
+      // Check exact paths
+      if (state.pathsList.includes(path)) {
+        return true;
+      }
 
-            // Define regex patterns for GUID segments
-            const guidPattern = '[0-9a-fA-F-]{36}';
+      // Define regex patterns for GUID segments
+      const guidPattern = '[0-9a-fA-F-]{36}';
 
-            // Check dynamic paths by comparing the base paths using regex
-            for (let route of state.pathsList) {
-                // Create a regex pattern for the current route
-                const dynamicPattern = new RegExp(`^${route}(\/${guidPattern})*(\/[^\/]+)*$`);
+      // Check dynamic paths by comparing the base paths using regex
+      for (let route of state.pathsList) {
+        // Create a regex pattern for the current route
+        const dynamicPattern = new RegExp(`^${route}(\/${guidPattern})*(\/[^\/]+)*$`);
 
-                if (dynamicPattern.test(path)) {
-                    return true;
-                }
-            }
-
-            return false;
+        if (dynamicPattern.test(path)) {
+          return true;
         }
-    },
-    actions: {
-        toggleRTL() {
-            if (this.language.value === 'ar') {
-                this.isRTL = true;
-                saveToLocalStorage('Rtl', this.isRTL);
-            } else {
-                this.isRTL = false;
-                saveToLocalStorage('Rtl', this.isRTL);
-            }
-        },
-        hasBranchIdKey(token) {
-            try {
-                const decodedToken = jwtDecode(token);
-                if (decodedToken && Object.prototype.hasOwnProperty.call(decodedToken, 'BranchId')) {
-                    return true;
-                }
-                return false;
-            } catch (error) {
-                return false;
-            }
-        },
-        changeLanguage(language) {
-            this.language = language;
-            saveToLocalStorage('Language', language);
-        },
-        async login(userPayload) {
-            //this.loading.setLoading();
-            // this.loading.setBarLoading();
-            try {
-                const response = await apiClient.post('/auth/login', userPayload);
-                const token = response.data.token;
+      }
 
-                // this.userInfo = response.data;
-                sessionStorage.setItem('userInfo', JSON.stringify(response.data));
-
-       
-                if (userPayload.rememberMe) {
-                    saveToLocalStorage('token', token);
-                    localStorage.setItem('rememberMe', userPayload.rememberMe);
-                    saveToLocalStorage('refreshToken', response.data.refreshToken);
-                } else {
-                    sessionStorage.setItem('Token', token);
-                    // localStorage.setItem('rememberMe', userPayload.rememberMe);
-                    localStorage.setItem('rememberMe', false);
-                }
-                await this.getMenu();
-                //this.loading.resetLoading();
-                // this.loading.resetBarLoading();
-
-                router.push({ name: 'available-branches' });
-            } catch (err) {
-                //this.loading.resetLoading();
-                // this.loading.resetBarLoading();
-                this.error = handleError(err, this.loading);
-            }
-        },
-        async chooseBranch(userPayload) {
-            try {
-                const response = await apiClient.post('/UserBranches/ChooseBranchFromAvailableBranches', userPayload);
-                const token = response.data.data;
-                var rememberMe = localStorage.getItem('rememberMe');
-                if (rememberMe == true) {
-                    saveToLocalStorage('token', token);
-                    localStorage.removeItem('refreshToken');
-                } else {
-                    sessionStorage.setItem('Token', token);
-                }
-                router.push({ name: 'userRoles' });
-            } catch (err) {
-                this.error = handleError(err, this.loading);
-            }
-        },
-        logout() {
-            const rememberMe = localStorage.getItem('rememberMe');
-            if (rememberMe == true) {
-                localStorage.removeItem('token');
-                localStorage.removeItem('refreshToken');
-                sessionStorage.removeItem('userInfo');
-            } else {
-                sessionStorage.removeItem('Token');
-            }
-            localStorage.removeItem('rememberMe');
-            this.pathsList = ['/pages/notfound', '/auth/login', '/auth/access', '/'];
-            this.pageTree = [];
-            router.push({ name: 'login' });
-        },
-        async getMenu() {
-            try {
-                const response = await apiClient.get('/PageRoles/GetMenuItems');
-                this.pageTree = removeEmptyItems(response.data.data.sidebarItems);
-                // const toValues = extractToValues(response.data.data);
-                // const routeList = extractRouteList(response.data.data);
-                this.pathsList = this.pathsList.concat(response.data.data.navigableItems);
-                this.routeList = response.data.data.searchItems;
-            } catch (err) {
-                console.log('Error occurred:', err);
-                this.error = handleError(err, this.loading);
-            }
-        }
+      return false;
     }
+  },
+  actions: {
+    toggleRTL() {
+      if (this.language.value === 'ar') {
+        this.isRTL = true;
+        saveToLocalStorage('Rtl', this.isRTL);
+      } else {
+        this.isRTL = false;
+        saveToLocalStorage('Rtl', this.isRTL);
+      }
+    },
+    hasBranchIdKey(token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        if (decodedToken && Object.prototype.hasOwnProperty.call(decodedToken, 'BranchId')) {
+          return true;
+        }
+        return false;
+      } catch (error) {
+        return false;
+      }
+    },
+    changeLanguage(language) {
+      this.language = language;
+      saveToLocalStorage('Language', language);
+    },
+    async login(userPayload) {
+      //this.loading.setLoading();
+      // this.loading.setBarLoading();
+      try {
+        const response = await apiClient.post('/auth/login', userPayload);
+        const token = response.data.token;
+
+        // this.userInfo = response.data;
+        sessionStorage.setItem('userInfo', JSON.stringify(response.data));
+
+        if (userPayload.rememberMe) {
+          saveToLocalStorage('token', token);
+          localStorage.setItem('rememberMe', userPayload.rememberMe);
+          saveToLocalStorage('refreshToken', response.data.refreshToken);
+        } else {
+          sessionStorage.setItem('Token', token);
+          // localStorage.setItem('rememberMe', userPayload.rememberMe);
+          localStorage.setItem('rememberMe', false);
+        }
+        await this.getMenu();
+        //this.loading.resetLoading();
+        // this.loading.resetBarLoading();
+
+        router.push({ name: 'available-branches' });
+      } catch (err) {
+        //this.loading.resetLoading();
+        // this.loading.resetBarLoading();
+        this.error = handleError(err, this.loading);
+      }
+    },
+    async chooseBranch(userPayload) {
+      try {
+        const response = await apiClient.post('/UserBranches/ChooseBranchFromAvailableBranches', userPayload);
+        const token = response.data.data;
+        var rememberMe = localStorage.getItem('rememberMe');
+        if (rememberMe == true) {
+          saveToLocalStorage('token', token);
+          localStorage.removeItem('refreshToken');
+        } else {
+          sessionStorage.setItem('Token', token);
+        }
+        router.push({ name: 'userRoles' });
+      } catch (err) {
+        this.error = handleError(err, this.loading);
+      }
+    },
+    logout() {
+      const rememberMe = localStorage.getItem('rememberMe');
+      if (rememberMe == true) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('refreshToken');
+        sessionStorage.removeItem('userInfo');
+      } else {
+        sessionStorage.removeItem('Token');
+      }
+      localStorage.removeItem('rememberMe');
+      this.pathsList = ['/pages/notfound', '/auth/login', '/auth/access', '/'];
+      this.pageTree = [];
+      router.push({ name: 'login' });
+    },
+    async getMenu() {
+      try {
+        const response = await apiClient.get('/PageRoles/GetMenuItems');
+        this.pageTree = removeEmptyItems(response.data.data.sidebarItems);
+        // const toValues = extractToValues(response.data.data);
+        // const routeList = extractRouteList(response.data.data);
+        this.pathsList = this.pathsList.concat(response.data.data.navigableItems);
+        this.routeList = response.data.data.searchItems;
+      } catch (err) {
+        console.log('Error occurred:', err);
+        this.error = handleError(err, this.loading);
+      }
+    }
+  }
 });
